feddeceb46b74153658d772b161b9b1aa4795a0e
nithiphon <nithiphon1712@gmail.com>
2026-01-22 00:00:48 +0700

    add

--- my-backend/server.js
+++ my-backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 // อนุญาตให้ Nuxt (localhost:3000) เรียก API ได้
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://test-backend-nuxt.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
